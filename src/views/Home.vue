090d2d62a8a23c2e9d54852075a45869c798a0ef
mewxz029 <mewlawbreakertv@gmail.com>
2021-11-10 23:08:30 +0700

    show books on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,47 @@
 <template>
-  <div id="home">
-    <h1 v-for="(item, index) in $store.state.books" :key="index">{{ item }}</h1>
-  </div>
+  <v-container>
+    <v-row no-gutters align-center>
+      <v-col class="pa-2" sm="6" md="3" v-for="item in books" :key="item._id">
+        <v-card class="pa-1" hover>
+          <v-img :src="item.imgUrl" max-height="300" width="100%" />
+          <v-btn color="indigo" outlined small class="ml-4 mt-3">
+            {{ item.category }}
+          </v-btn>
+          <v-card-title class="headline">
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <h4
+                  class="ellipsis"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  {{ item.title }}
+                </h4>
+              </template>
+              <span>{{ item.title }}</span>
+            </v-tooltip>
+            <v-spacer />
+            <v-chip outlined color="success">$ {{ item.price }}</v-chip>
+          </v-card-title>
+          <v-card-text class="py-0">
+            <div class="des d-flex">
+              <p class="text-truncate">{{ item.desc }}</p>
+            </div>
+            <p>
+              จำนวนคงเหลือ : <v-chip>{{ item.stock }}</v-chip>
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // import axios from "axios";
+import { mapState } from "vuex";
 
 export default {
   name: "home",
@@ -16,5 +52,22 @@ export default {
   mounted() {
     this.$store.dispatch("getBooks");
   },
+  computed: {
+    ...mapState({
+      books: (state) => state.books,
+    }),
+  },
 };
 </script>
+
+<style>
+.ellipsis {
+  width: 130px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+.des {
+  height: 100px;
+}
+</style>
